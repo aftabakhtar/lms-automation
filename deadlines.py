a55e3ce4ce65e4682b79797939aa5610036acd40
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2020-10-23 17:49:52 +0500

    Implement username and password passing

--- deadlines.py
+++ deadlines.py
@@ -1,6 +1,26 @@
+import sys
+import getpass
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+
+if sys.stdin.isatty():
+   print("Enter credentials")
+   user = input("Username: ")
+   pwd = getpass.getpass("Password: ")
+else:
+    print("Please make sure you are using a terminal")
+    sys.exit(0)
+
+
 driver = webdriver.Chrome('./chromedriver')
-driver.get("https://www.python.org/")
-driver.quit()
+driver.get("https://lms.nust.edu.pk/portal/")
+
+username = driver.find_element_by_xpath('//input[@id="login_username"]')
+username.send_keys(user)
+
+password = driver.find_element_by_xpath('//input[@id="login_password"]')
+password.send_keys(pwd)
+
+button = driver.find_element_by_xpath('//*[@id="login"]/div[4]/input')
+button.click()
