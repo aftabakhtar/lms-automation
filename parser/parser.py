2fda01a2fb0fb0d3e98622710174dc62e173d900
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2020-11-14 04:11:46 +0500

    Re-design to work with lms update

--- parser/parser.py
+++ parser/parser.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import datetime
 import requests
@@ -7,41 +8,64 @@ from terminaltables import AsciiTable
 
 def parse(user, pwd):
     courses = [['Course', 'Assignment', 'Due Date', 'Done']]
-    url = 'https://lms.nust.edu.pk/portal/login/index.php'
-    response = requests.post(url, data={'username': user,
-                                           'password': pwd,
-                                           'rememberusername': '0'})
+    login_url = 'https://lms.nust.edu.pk/portal/login/index.php?login'
+
+    s = requests.Session()
+    
+
+    response = s.get(login_url)
+    # cookie = response.cookies.get_dict()
+    pattern = '<input type="hidden" name="logintoken" value="\w{32}">'
+    token = re.findall(pattern, response.text)
+    token = re.findall("\w{32}", token[0])
+
+    payload = {'username': user, 'password': pwd, 'anchor': '', 'logintoken': token[0]}
+
+    response = s.post(login_url, data=payload)
+
     html_source = response.text
     tree = html.fromstring(html=html_source)
-    course_list = tree.xpath('//div[@id="1"]/div[@id="course_list"]/div')
-    
-    if len(course_list) == 0:
+    if 'Log in to the site' in str(tree.xpath('//head/title/text()')):
         print("Invalid Credentials")
         sys.exit(0)
-    
-    for course in course_list:
-        course_name = course.xpath('//div[@id="' + course.get('id') +'"]/div[@class="course_title"]/h2/a/text()')[0].split(' BSCS2k18',1)[0]
-        assignments_list = course.xpath('//div[@id="' + course.get('id') +'"]/div[@class="activity_info"]//div[@class="name"]/a/text()')
-        assignments_list = [str(i) for i in assignments_list if not "Forum Post" in str(i)]
-        due_dates = course.xpath('//div[@id="' + course.get('id') +'"]/div[@class="activity_info"]//div[@class="info"]/text()')
-        status = course.xpath('//div[@id="' + course.get('id') +'"]/div[@class="activity_info"]//div[@class="details"]/text()')
-        due_dates = [str(i) for i in due_dates if "Due date:" in str(i)]
 
-        for i in range(len(assignments_list)):
-            current_course = []
-            current_course += [str(course_name[6:])]
-            assignment_name = str(assignments_list[i])
-            current_course += [assignment_name[0:17]]
-            date = due_dates[i]
-            current_course += [datetime.datetime.strptime(date[10:],"%A, %d %B %Y, %I:%M %p").strftime("%a, %d %b %y, %I:%M%p")]
-            
-            if str(status[i]) == "My submission: Submitted for grading, Not graded":
-                detail = u'\u2714'
-            else:
-                detail = u'\u2716'
-            
-            current_course += [detail]
-            courses += [current_course]
+
+    upcoming_url = 'https://lms.nust.edu.pk/portal/calendar/view.php?view=upcoming'
+    response = s.get(upcoming_url)
+    html_source = response.text
+    tree = html.fromstring(html=html_source)
+    href_links = tree.xpath('//a[@class="card-link"]')
+    href_links = [i.get('href') for i in href_links]
+    
+    print('Fetching data, please wait...\n')
+    
+    for link in href_links:
+        course_description = []
+        response = s.get(link)
+        html_source = response.text
+        tree = html.fromstring(html=html_source)
+        course_name = str(tree.xpath('//div[@class="page-header-headings"]/h1/text()'))
+        assignment_title = tree.xpath('//div[@role="main"]/h2/text()')
+        table_information = tree.xpath('//table[@class="generaltable"]//td/text()')
+
+        if 'This is attempt' in table_information[0]:
+            submission_status = table_information[1]
+            due_date = table_information[3]
+        else:
+            submission_status = table_information[0]
+            due_date = table_information[2]
+
+        course_description += [course_name[8:-13]]
+        course_description += [assignment_title[0][0:17]]
+        course_description += [datetime.datetime.strptime(due_date,"%A, %d %B %Y, %I:%M %p").strftime("%a, %d %b %y, %I:%M%p")]
+        
+        if str(submission_status) == 'Submitted for grading':
+            submission_status = u'\u2714'
+        else:
+            submission_status = u'\u2716'
+
+        course_description += [submission_status]
+        courses += [course_description]
 
     table = AsciiTable(courses)
     print(table.table)
